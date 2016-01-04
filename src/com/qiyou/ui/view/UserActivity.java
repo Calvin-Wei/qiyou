8893bca799e13b741210ed19d4e44497a0d9d074
wxc <519462367@qq.com>
2016-01-04 18:43:31 +0800

    下拉刷新未完成

--- src/com/qiyou/ui/view/UserActivity.java
+++ src/com/qiyou/ui/view/UserActivity.java
@@ -5,6 +5,8 @@ package com.qiyou.ui.view;
 
 import com.qiyou.R;
 import com.qiyou.adapter.Adapter_GridView;
+import com.qiyou.team.Team_Manage;
+import com.qiyou.ticket.Ticket_Manage;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -29,10 +31,12 @@ public class UserActivity extends Activity implements OnClickListener {
 	// private GridView my_gridView_user;
 	// private Adapter_GridView adapter_GridView;
 
-	private LinearLayout ll_user_life;
-	private LinearLayout ll_user_members;
-	private LinearLayout ll_user_hotel;
-	private LinearLayout ll_user_opinion;
+	private LinearLayout ll_user_jingdian;// 景点优惠券
+	private LinearLayout ll_user_members;// 会员卡
+	private LinearLayout ll_user_hotel;// 酒店优惠券
+	private LinearLayout ll_user_opinion;// 关于我们
+	private LinearLayout my_team_manage;// 我的团队管理
+	private LinearLayout my_ticket;// 我的门票
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +49,24 @@ public class UserActivity extends Activity implements OnClickListener {
 	 * 
 	 */
 	private void initView() {
-		ll_user_life = (LinearLayout) findViewById(R.id.ll_user_life);
+		ll_user_jingdian = (LinearLayout) findViewById(R.id.ll_user_jingdian);
 		ll_user_members = (LinearLayout) findViewById(R.id.ll_user_members);
 		ll_user_hotel = (LinearLayout) findViewById(R.id.ll_user_hotel);
 		ll_user_opinion = (LinearLayout) findViewById(R.id.ll_user_opinion);
-		ll_user_life.setOnClickListener(this);
+		my_team_manage = (LinearLayout) findViewById(R.id.my_team_manage);
+		my_ticket = (LinearLayout) findViewById(R.id.my_ticket);
+		ll_user_jingdian.setOnClickListener(this);
 		ll_user_members.setOnClickListener(this);
 		ll_user_hotel.setOnClickListener(this);
 		ll_user_opinion.setOnClickListener(this);
+		my_team_manage.setOnClickListener(this);
+		my_ticket.setOnClickListener(this);
 		// my_gridView_user=(GridView) view.findViewById(R.id.gridView_user);
 	}
 
 	public void onClick(View arg0) {
-		// switch (arg0.getId()) {
-		// case R.id.ll_user_life:
+		switch (arg0.getId()) {
+		// case R.id.ll_user_jingdian:
 		// Intent intent1=new Intent(getActivity(),User_life.class);
 		// startActivity(intent1);
 		// break;
@@ -74,10 +82,19 @@ public class UserActivity extends Activity implements OnClickListener {
 		// Intent intent4=new Intent(getActivity(),User_opinion.class);
 		// startActivity(intent4);
 		// break;
-		//
-		// default:
-		// break;
-		// }
+		case R.id.my_team_manage:
+			Intent team_intent = new Intent(UserActivity.this,
+					Team_Manage.class);
+			startActivity(team_intent);
+			break;
+		case R.id.my_ticket:
+			Intent ticket_intent = new Intent(UserActivity.this,
+					Ticket_Manage.class);
+			startActivity(ticket_intent);
+
+		default:
+			break;
+		}
 
 	}
 }
